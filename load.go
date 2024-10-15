8d12d0de4be11d7cb8194fcb929217434417a0ad
yangkequn <yangkequn@gmail.com>
2024-10-15 20:09:30 +0800

    better appearance of log

--- load.go
+++ load.go
@@ -7,12 +7,12 @@ import (
 )
 
 func LoadToml(keyname string, configObj interface{}) {
-	logger.Info().Str("Config Item ", keyname).Msg(" loading ..")
+	logger.Info().Str("Config Item", keyname).Str("data", "loading ..").Send()
 	//step1: load config from file
 	utils.LoadFromFile(keyname, configObj)
 	//step2: load config from env. this will overwrite the config from file
 	//step3: load config from web. this will overwrite the config from env.
 	//warning local config will be overwritten by the config from web, to prevent falldown of config from web.
 	cfgurl.LoadFromUrl(keyname, configObj)
-	logger.Info().Str("Config Item", keyname).Str("json", utils.ToHidePswdString(configObj)).Send()
+	logger.Info().Str("Config Item", keyname).Str("data", utils.ToHidePswdString(configObj)).Send()
 }
